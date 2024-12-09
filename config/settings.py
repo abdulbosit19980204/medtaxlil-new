34b922dfb990484d9d9e0928bf39085c0cffd7ed
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-12-09 12:23:24 +0500

    Postgres

--- config/settings.py
+++ config/settings.py
@@ -91,10 +91,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'medtaxlil',
+        'USER': 'medadmin',
+        'PASSWORD': 'med1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
