ce55679c130c59afd24d02e06bc9c4efc01f0662
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-12-09 23:10:53 +0500

    requirements cors headers

--- config/settings.py
+++ config/settings.py
@@ -70,15 +70,19 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = ['*'
-                        # "http://localhost:3000",  # Frontend domeni
-                        # "http://api.med-t",
-                        ]
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8000",
+    "localhost:8000",
+    "api.med-taxlil.uz",
+    "www.med-taxlil.uz",
+    "165.22.245.188",
+]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = [
     'content-type',
     'authorization',
     'x-custom-header',
+    '*'
 ]
 CORS_ALLOW_METHODS = [
     "GET",
