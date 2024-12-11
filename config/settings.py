49b9180bddd2ce7396782083a7004e999588581f
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-12-11 12:56:32 +0500

    Append Status

--- config/settings.py
+++ config/settings.py
@@ -180,7 +180,7 @@ TIME_ZONE = 'Asia/Tashkent'
 USE_I18N = True
 
 USE_TZ = True
-APPEND_SLASH = False
+# APPEND_SLASH = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
