639dcfc083638f024c73d84786d38d31bdedae7a
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-12-11 12:51:55 +0500

    highest rate

--- config/settings.py
+++ config/settings.py
@@ -180,10 +180,10 @@ TIME_ZONE = 'Asia/Tashkent'
 USE_I18N = True
 
 USE_TZ = True
+APPEND_SLASH = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
